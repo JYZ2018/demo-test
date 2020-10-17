f8cba0ec1a29b90a84885ba0d777b289a2b69792
JYZ2018 <44782282+JYZ2018@users.noreply.github.com>
2020-10-16 20:44:38 -0400

    Add files via upload

---
+++ test_3.py
@@ -0,0 +1,44 @@
+b = ['I', '超', '喜', '欢', 'YOU']
+c = ''
+print(c.join(b))
+
+#   第二组：
+a = ('I'+'超'+'喜'+'欢'+'YOU')
+print(a)
+print(type(a))
+
+a = ((('I'+'超'+'喜'+'欢'+'YOU')))
+print(a)
+print(type(a))
+
+a = 'I'+'超'+'喜'+'欢'+'YOU'
+print(a)
+print(type(a))
+
+b = ('I'+'超'+'喜'+'欢'+'YOU')
+c = ''
+print(c.join(b))
+
+#   第二组：
+a = ['I', '超', '喜', '欢', 'YOU']
+print(a)
+print(type(a))
+
+a='1'
+print(a)
+
+a=('1')
+print(a)
+print(type(a))
+
+a="1'1'"
+print(a)
+print(type(a))
+
+import random
+
+a=random.randomint()
+
+a="'1'"
+print(a)
+print(type(a))
